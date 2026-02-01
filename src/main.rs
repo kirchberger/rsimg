92161eea08dd28fd4e28416d8bf72ae6eb4c3c54
kirchberger <kirchbergeralex0@gmail.com>
2026-02-01 11:28:00 -0500

    added simple argument verification and polling for user input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
-use std::env;
-use std::io;
+// Will slowly reduce external dependancies
+use std::{env,io,string,time::Duration};
+use crossterm::{event::poll};
+//struct image
 
-
-fn main() -> std::io::Result<()> {
-    for argument in env::args() {
-            println!("{argument}");
+fn usr_cancel() -> Result<(),()> {
+    loop {
+        if  poll(Duration::from_millis(100)).unwrap() {
+            return Ok(());
+        }
     }
+}
 
+fn main() -> Result<(),bool> {
+    
+    let args : Vec<String> = env::args().collect(); // collect arguments
+    let arglen = args.len();
+    
+    if arglen != 2 { // check number of arguments
+        //return Err(String::from("Incorrect number of arguments"));
+        return Ok(());
+    }
+    
+    // decode image 
+    
+    // down size image to correct resolution
+
+    // render image
+
+    // wait for cancel input
+    loop {
+        if  poll(Duration::from_millis(100)).unwrap() {
+            break;
+        }
+    }
+    
     Ok(())
 }
